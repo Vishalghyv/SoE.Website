c8d69dc7d8f9f1cb10ef4db9cd9a4d9732aa786e
Umesh Kumar <umesh98_soe@jnu.ac.in>
2020-11-24 17:51:20 +0530

    added footer and header file

---
+++ footer.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="IE=edge, chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+
+  <link rel="icon" href="assets/favicon.ico">
+  <title>School of Engineering &middot; JNU</title>
+  <!-- fontawesome icon css -->
+  <link rel="stylesheet" href="fontawesome/css/all.min.css">
+  <!-- google font-family -->
+  <link href='https://fonts.googleapis.com/css?family=Charm' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Charmonman' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Ceviche One' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Carter One' rel='stylesheet'>
+  <!-- bootstrap-4 css -->
+  <link rel="stylesheet" href="libs/bootstrap-4.5.3.css">
+  <!-- main css -->
+  <link rel="stylesheet" href="css/styles.css?v=<?php echo time(); ?>">
+  <!-- <link rel="stylesheet" href="css/styles.css"> -->
+  <!-- swiper.js css -->
+  <link rel="stylesheet" href="css/swiper-bundle-min.css">
+  <!-- <link rel="stylesheet" href="css/swiper-bundle.css"> -->
+  <!-- owl-corousel css-->
+  <link rel="stylesheet" href="owlcarousel/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="owlcarousel/assets/owl.theme.default.min.css">
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css"> -->
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css"> -->
+</head>
+<style>
+
+</style>
+
+<body>
+  <footer class="footer">
+    <div class="container p-2 p-md-3 bottom_border">
+      <div class="row">
+        <div class="col col-md-8">
+          <div class="row">
+            <div class="col-sm-6 col-12 col">
+              <h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
+              <!--headin5_amrc-->
+              <p class="mb10">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem
+                Ipsum
+                has been the industry's standard dummy text ever since the 1500s</p>
+              <p><i class="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35 </p>
+              <p><i class="fa fa-phone"></i> +91-9999878398 </p>
+              <p><i class="fa fa fa-envelope"></i> info@example.com </p>
+
+
+            </div>
+
+
+            <div class="col-sm-6 col-12 col">
+              <h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+              <!--headin5_amrc-->
+              <ul class="footer_ul_amrc">
+                <li><a href="http://webenlance.com">Image Rectoucing</a></li>
+                <li><a href="http://webenlance.com">Clipping Path</a></li>
+                <li><a href="http://webenlance.com">Hollow Man Montage</a></li>
+                <li><a href="http://webenlance.com">Ebay & Amazon</a></li>
+                <li><a href="http://webenlance.com">Hair Masking/Clipping</a></li>
+                <li><a href="http://webenlance.com">Image Cropping</a></li>
+              </ul>
+              <!--footer_ul_amrc ends here-->
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-6 col-12 col">
+              <h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+              <!--headin5_amrc-->
+              <ul class="footer_ul_amrc">
+                <li><a href="http://webenlance.com">Remove Background</a></li>
+                <li><a href="http://webenlance.com">Shadows & Mirror Reflection</a></li>
+                <li><a href="http://webenlance.com">Logo Design</a></li>
+                <li><a href="http://webenlance.com">Vectorization</a></li>
+                <li><a href="http://webenlance.com">Hair Masking/Clipping</a></li>
+                <li><a href="http://webenlance.com">Image Cropping</a></li>
+              </ul>
+              <!--footer_ul_amrc ends here-->
+            </div>
+
+
+            <div class="col-sm-6 col-12 col">
+              <h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+              <!--headin5_amrc ends here-->
+
+              <ul class="footer_ul2_amrc">
+                <li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+                  <p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a>
+                  </p>
+                </li>
+                <li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+                  <p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a>
+                  </p>
+                </li>
+                <li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+                  <p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a>
+                  </p>
+                </li>
+              </ul>
+              <!--footer_ul2_amrc ends here-->
+            </div>
+          </div>
+        </div>
+        <div class="col col-md-4 p-0 d-flex align-items-center justify-content-center">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.9830929899003!2d77.16402671440491!3d28.540227895004897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1dc0ea0183b7%3A0xefd8d19ac7f061e7!2sJawaharlal%20Nehru%20University!5e0!3m2!1sen!2sin!4v1604925043200!5m2!1sen!2sin" width="350" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="container">
+      <ul class="foote_bottom_ul_amrc">
+        <li><a href="http://webenlance.com">Home</a></li>
+        <li><a href="http://webenlance.com">About</a></li>
+        <!-- <li><a href="http://webenlance.com">Services</a></li>
+                <li><a href="http://webenlance.com">Pricing</a></li> -->
+        <li><a href="http://webenlance.com">Blog</a></li>
+        <li><a href="http://webenlance.com">Contact</a></li>
+      </ul>
+      <!--foote_bottom_ul_amrc ends here-->
+      <p class="text-center">Copyright @2017 | Designed With by <a href="#">Your Company Name</a></p>
+
+      <ul class="social_footer_ul">
+        <li><a href="http://webenlance.com"><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="http://webenlance.com"><i class="fab fa-twitter"></i></a></li>
+        <li><a href="http://webenlance.com"><i class="fab fa-linkedin"></i></a></li>
+        <li><a href="http://webenlance.com"><i class="fab fa-instagram"></i></a></li>
+      </ul>
+      <!--social_footer_ul ends here-->
+    </div>
+  </footer>
+</body>
+
+</html>
